cb0b7da026a77acc07edfa325186193b03d8c55a
Cantley <jcantley@edisonschools.net>
2019-03-06 13:28:54 -0500

    Initial grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        uglify: {
+          my_target: {
+            files: {
+              'main.min.js': ['app/**/*.js']
+            }
+          }
+        },
+        less: {
+            development: {
+              files: {
+                'assets/css/main.css': ['assets/less/main.less']
+              }
+            },
+        },
+        watch: {
+            js: {
+              files: ['**/*.js'],
+              tasks: ['uglify'],
+              options: {
+                spawn: false,
+              },
+            },
+            css: {
+              files: ['**/*.css'],
+              tasks: [''],
+              options: {
+                spawn: false,
+              },
+            },
+            less: {
+              files: ['**/*.less'],
+              tasks: ['less'],
+              options: {
+                spawn: false,
+              },
+            },
+            html: {
+              files: ['**/*.html'],
+              tasks: [''],
+              options: {
+                spawn: false,
+              },
+            },
+          },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['uglify', 'less', 'watch']);
+};
