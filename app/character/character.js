c996d6b0e28b88eb94258bdf758f8d60c1bdd72a
Cantley <jcantley@edisonschools.net>
2019-03-07 11:07:29 -0500

    finished adding all items from phb to items'json

--- app/character/character.js
+++ app/character/character.js
@@ -1,40 +1,89 @@
 angular.module("carryingCapacity")
-.controller("charCtrl", ["$scope", "$log", function($scope, $log) {
+.controller("charCtrl", ["$scope", "$http", "$log", function($scope, $http, $log) {
     $scope.currentItems = [];
     $scope.currentWeapons = [];
     $scope.currentArmor = [];
     $scope.currentPotions = [];
+    $scope.currentAmmunition = [];
+    $scope.currentClothing = [];
+    $scope.currentArcaneFocus = [];
+    $scope.currentDruidicFocus = [];
+    $scope.currentHolySymbols = [];
+    $scope.currentArtisansTools = [];
+    $scope.currentGamingSets = [];
+    $scope.currentKits = [];
+    $scope.currentMusicalInstruments = [];
     $scope.load = 0;
-    $scope.itemList = [{name: "Pike", weight: 18, type: "weapon"},
-                       {name: "Crossbow, heavy", weight: 18, type: "weapon"},
-                       {name: "Plate", weight: 65, type: "armor"}, 
-                       {name: "Shield", weight: 6, type: "armor"}, 
-                       {name: "Potion of Healing", weight: 0.5, type: "potion"},
-                       {name: "Potion of Growth", weight: 0.5, type: "potion"}];
+    $http({
+        method: 'GET',
+        url: '/items.json'
+    }).then(function(response) {
+        $scope.itemList = response.data;
+    });
 
     function findWeapons() {
-        $scope.currentWeapons = $scope.currentItems.filter(function(item) {
-            return item.type === "weapon";
-        });
+        $scope.currentWeapons = $scope.currentItems.filter(item => item.type === "weapon");
     }
 
     function findArmor() {
-        $scope.currentArmor = $scope.currentItems.filter(function(item) {
-            return item.type === "armor";
-        });
+        $scope.currentArmor = $scope.currentItems.filter(item => item.type === "armor");
     }
 
     function findPotions() {
-        $scope.currentPotions = $scope.currentItems.filter(function(item) {
-            return item.type === "potion";
-        });
+        $scope.currentPotions = $scope.currentItems.filter(item => item.type === "potion");
+    }
+
+    function findAmmunition() {
+        $scope.currentAmmunition = $scope.currentItems.filter(item => item.type === "ammunition");
+    }
+
+    function findClothing() {
+        $scope.currentClothing = $scope.currentItems.filter(item => item.type === "clothing");
+    }
+
+    function findDruidicFocus() {
+        $scope.currentDruidicFocus = $scope.currentItems.filter(item => item.type === "druidic_focus");
+    }
+
+    function findArcaneFocus() {
+        $scope.currentArcaneFocus = $scope.currentItems.filter(item => item.type === "arcane_focus");
+    }
+
+    function findHolySymbol() {
+        $scope.currentHolySymbols = $scope.currentItems.filter(item => item.type === "holy_symbol");
+    }
+
+    function findArtisansTools() {
+        $scope.currentArtisansTools = $scope.currentItems.filter(item => item.type === "artisans_tools");
+    }
+
+    function findGamingSet() {
+        $scope.currentGamingSets = $scope.currentItems.filter(item => item.type === "gaming_set");
+    }
+
+    function findKits() {
+        $scope.currentKits = $scope.currentItems.filter(item => item.type === "kit");
+    }
+
+    function findMusicalInstruments() {
+        $scope.currentMusicalInstruments = $scope.currentItems.filter(item => item.type === "musical_instrument");
     }
 
     $scope.addItem = function(item) {
+        $log.log(item);
         $scope.currentItems.push(item);
-        $scope.load += item.weight;
+        $scope.load += parseInt(item.weight);
         findWeapons();
         findArmor();
         findPotions();
+        findAmmunition();
+        findClothing();
+        findDruidicFocus();
+        findArcaneFocus();
+        findHolySymbol();
+        findArtisansTools();
+        findGamingSet();
+        findKits();
+        findMusicalInstruments();
     };
 }]);
