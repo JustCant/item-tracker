b1c1e0890652f4e9b32fd3acc3fbe905347dc4b4
Cantley <jcantley@edisonschools.net>
2019-03-09 11:08:15 -0500

    removed duplicate items and added multiple item indicator

--- app/character/character.js
+++ app/character/character.js
@@ -24,16 +24,40 @@ angular.module("carryingCapacity")
         kit: [],
         musical_instrument: [],
         miscellaneous: []
-    }
+    }//end currentItems
 
     $scope.load = 0;
 
     function assignByType(list, item) {
         list[item.type].push(item);
-    }
+        $log.log(item);
+    }//end assignByType
      
     $scope.addItem = function(item) {
         $scope.load += parseFloat(item.weight);
-        assignByType($scope.currentItems, item);        
-    };
+        if(!item.quantity)
+            item.quantity = 1;
+        for(let key in $scope.currentItems) {
+            if(item.type === key) {
+                if($scope.currentItems[key].length >= 1) {
+                    $scope.currentItems[key].forEach(x => {
+                        if(item.name === x.name) 
+                            x.quantity += 1;
+                        else
+                            assignByType($scope.currentItems, item); 
+                    }); 
+                } else 
+                    assignByType($scope.currentItems, item);
+            }//end if
+        }//end for
+    };//end addItem
+
+    $scope.removeItem = function(item, array) {
+        $scope.load -= parseFloat(item.weight);
+        item.quantity -= 1;
+        $log.log(item.quantity);
+        if(item.quantity === 0)    
+            array.splice(array.indexOf(item), 1);
+        $log.log(item.quantity);
+    };//end removeItem
 }]);
