5a15b7c06588eb8ea03fa245eca442487c5a48c7
Cantley <jcantley@edisonschools.net>
2019-03-11 10:23:10 -0400

    Fixed bug that incorectly added items to the current items list

--- app/character/character.js
+++ app/character/character.js
@@ -33,18 +33,27 @@ angular.module("carryingCapacity")
     }//end assignByType
      
     $scope.addItem = function(item) {
+        //Add item weight to $scope.load
         $scope.load += parseFloat(item.weight);
+
+        //If item does not have a quantity property add one
         if(!item.quantity)
             item.quantity = 1;
+
+        //Loop through properties of $scope.currentItems
         for(let key in $scope.currentItems) {
+            //find appropriate array
             if(item.type === key) {
+                //check if array is empty or not
                 if($scope.currentItems[key].length >= 1) {
+                    //check if item is already in array
                     $scope.currentItems[key].forEach(x => {
-                        if(item.name === x.name) 
-                            x.quantity += 1;
-                        else
-                            assignByType($scope.currentItems, item); 
-                    }); 
+                        if(x.name === item.name) 
+                            x.quantity += 1;   
+                        //if at the end of the array with no match
+                        else if (($scope.currentItems[key].indexOf(x) + 1) === $scope.currentItems[key].length)  
+                            assignByType($scope.currentItems, item);                   
+                    });                      
                 } else 
                     assignByType($scope.currentItems, item);
             }//end if
@@ -54,9 +63,7 @@ angular.module("carryingCapacity")
     $scope.removeItem = function(item, array) {
         $scope.load -= parseFloat(item.weight);
         item.quantity -= 1;
-        $log.log(item.quantity);
         if(item.quantity === 0)    
             array.splice(array.indexOf(item), 1);
-        $log.log(item.quantity);
     };//end removeItem
 }]);
